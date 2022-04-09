9db33fecbf53824f66bd083ee57149c40e106a84
PoChin cheng <pcheng@clark.edu>
2022-04-08 23:48:08 -0700

    displays 300 rounder stars

--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ const stars = 300
 for (let i = 0; i < stars; i++) {
     let x = Math.random() * canvas.offsetWidth
     let y = Math.random() * canvas.offsetWidth
-    context.fillStyle = "white"
-    context.fillRect(x,y,1,1)
+    let r = Math.random() * 1.5
+    context.beginPath()
+    context.arc(x, y, r, 0, 360)
+    context.fillStyle = "hsla(200, 100%, 50%, 0.8)"
+    context.fill()
 }
