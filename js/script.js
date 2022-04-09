111be592ce49f7f9e12a2b52c9bb74425b590fca
PoChin cheng <pcheng@clark.edu>
2022-04-09 00:31:18 -0700

    displays 2000 warmer color stars on 1000x600 canvas where style to take up the whole width of the page

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 const canvas = document.getElementById("starfield")
 const context = canvas.getContext("2d")
-const stars = 300
+const stars = 2000
+const colorRange = [0, 90, 240]
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
 for (let i = 0; i < stars; i++) {
     let x = Math.random() * canvas.offsetWidth
     let y = Math.random() * canvas.offsetWidth
-    let r = Math.random() * 1.5
+    let r = Math.random() * 1.7
+    let hue = colorRange[getRandom(0, colorRange.length -1)]
+    let sat = getRandom(25, 75)
     context.beginPath()
     context.arc(x, y, r, 0, 360)
-    context.fillStyle = "hsla(200, 100%, 50%, 0.8)"
+    context.fillStyle = `hsl(${hue}, ${sat}%, 88%)`
     context.fill()
+    console.log(i)
 }
